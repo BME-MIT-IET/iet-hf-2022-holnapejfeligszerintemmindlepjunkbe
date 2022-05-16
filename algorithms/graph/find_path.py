7df704f1bb25f219394f76d48f9d6d273695d07a
Brknyi <57301527+Brknyi@users.noreply.github.com>
2022-05-16 23:08:14 +0200

    Change the boolean comparison operator
    The opposite comparison is less complex.

--- algorithms/graph/find_path.py
+++ algorithms/graph/find_path.py
@@ -10,7 +10,7 @@ def find_path(graph, start, end, path=[]):
     path = path + [start]
     if start == end:
         return path
-    if not start in graph:
+    if start not in graph:
         return None
     for node in graph[start]:
         if node not in path:
